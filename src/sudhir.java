3ea35467258f83266b1cfd7c95b35ed7c5059fd5
sudhir <sudhirkumar9gf@gmail.com>
2021-08-03 18:13:01 +0530

    abc

---
+++ src/sudhir.java
@@ -0,0 +1,6 @@
+
+public class sudhir {
+public static void main(String[] args) {
+	System.out.println("sudhir");
+}
+}
---
+++ src/sudhir_pkg_by_class/aaaaaaaaa.java
@@ -0,0 +1,10 @@
+package sudhir_pkg_by_class;
+
+public class aaaaaaaaa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
