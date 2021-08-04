4b154bbcfe324e136e33b4f4eaf003b9d9b0894c
sudhir <sudhirkumar9gf@gmail.com>
2021-08-04 12:08:22 +0530

    sudhir changes

---
+++ src/sudhir_payments/object_and_class.java
@@ -0,0 +1,14 @@
+package sudhir_payments;
+
+public class object_and_class{  
+	 int id;  
+	 String name;  
+	}  
+	class TestStudent2{  
+	 public static void main(String args[]){  
+	  object_and_class s1=new object_and_class();  
+	  s1.id=101;  
+	  s1.name="Sonoo";  
+	  System.out.println(s1.id+" "+s1.name);//printing members with a white space  
+	 }  
+	}  
---
+++ src/sudhir_payments/sddddddddddd.java
@@ -0,0 +1,5 @@
+package sudhir_payments;
+
+public class sddddddddddd {
+
+}
